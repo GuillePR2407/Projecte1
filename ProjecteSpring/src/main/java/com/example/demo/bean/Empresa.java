1a855163ec3f9e469366e89ae0c38c31a03bb6ff
GuillePR2407 <guillempr2003@gmail.com>
2023-10-31 16:30:19 +0100

    aaaaa

--- ProjecteSpring/src/main/java/com/example/demo/bean/Empresa.java
+++ ProjecteSpring/src/main/java/com/example/demo/bean/Empresa.java
@@ -1,15 +1,24 @@
 package com.example.demo.bean;
 
 import jakarta.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Empresa {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
+    private @Id @GeneratedValue Long id;
     private String nombre;
     private String descripcion;
-    // Otros atributos y getters/setters
+    
+    
+    Empresa() {}
+    
+    Empresa(String nombre, String descripcion) {
+
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    
 	public Long getId() {
 		return id;
 	}
@@ -28,6 +37,28 @@ public class Empresa {
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+	
+	@Override
+	  public boolean equals(Object o) {
+
+	    if (this == o)
+	      return true;
+	    if (!(o instanceof Empresa))
+	      return false;
+	    Empresa empresa = (Empresa) o;
+	    return Objects.equals(this.id, empresa.id) && Objects.equals(this.nombre, empresa.nombre)
+	        && Objects.equals(this.descripcion, empresa.descripcion);
+	  }
+	
+	 @Override
+	  public int hashCode() {
+	    return Objects.hash(this.id, this.nombre, this.descripcion);
+	  }
+
+	  @Override
+	  public String toString() {
+	    return "Empresa{" + "id=" + this.id + ", nombre='" + this.nombre + '\'' + ", descripcion='" + this.descripcion + '\'' + '}';
+	  }
     
 }
 
--- ProjecteSpring/src/main/java/com/example/demo/controller/EmpresaController.java
+++ ProjecteSpring/src/main/java/com/example/demo/controller/EmpresaController.java
@@ -1,63 +1,68 @@
 package com.example.demo.controller;
 
-import java.util.Optional;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.bean.Empresa;
 import com.example.demo.bean.EmpresaRepository;
+import com.example.demo.bean.EmpresaNotFoundException;
 
 @RestController
-@RequestMapping("/api/empresas")
-public class EmpresaController {
-    private final EmpresaRepository empresaRepository;
+class EmpresaController {
 
-    @Autowired
-    public EmpresaController(EmpresaRepository empresaRepository) {
-        this.empresaRepository = empresaRepository;
-    }
+  private final EmpresaRepository repository;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Empresa> consultarEmpresa(@PathVariable Long id) {
-        // Implementa la lógica para consultar una empresa por su ID
-    	 Optional<Empresa> empresa = empresaRepository.findById(id);
-         if (empresa.isPresent()) {
-             return ResponseEntity.ok(empresa.get());
-         } else {
-             return ResponseEntity.notFound().build();
-         }
-    }
+  EmpresaController(EmpresaRepository repository) {
+    this.repository = repository;
+  }
 
-    @PostMapping
-    public ResponseEntity<Empresa> agregarEmpresa(@RequestBody Empresa empresa) {
-        // Implementa la lógica para agregar una empresa
-    	Empresa nuevaEmpresa = empresaRepository.save(empresa);
-        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaEmpresa);
-    }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Empresa> modificarEmpresa(@PathVariable Long id, @RequestBody Empresa empresa) {
-        // Implementa la lógica para modificar una empresa
-    	if (empresaRepository.existsById(id)) {
-            empresa.setId(id);
-            Empresa empresaModificada = empresaRepository.save(empresa);
-            return ResponseEntity.ok(empresaModificada);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+  // Aggregate root
+  // tag::get-aggregate-root[]
+  @GetMapping("/empresas")
+  List<Empresa> all() {
+    return repository.findAll();
+  }
+  // end::get-aggregate-root[]
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminarEmpresa(@PathVariable Long id) {
-        // Implementa la lógica para eliminar una empresa
-    	if (empresaRepository.existsById(id)) {
-            empresaRepository.deleteById(id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+  @PostMapping("/empresas")
+  Empresa newEmpresa(@RequestBody Empresa newEmpresa) {
+    return repository.save(newEmpresa);
+  }
+
+  // Single item
+  
+  @GetMapping("/empresas/{id}")
+  Empresa one(@PathVariable Long id) {
+    
+    return repository.findById(id)
+      .orElseThrow(() -> new EmpresaNotFoundException(id));
+  }
+
+  @PutMapping("/empresas/{id}")
+  Empresa replaceEmpresa(@RequestBody Empresa newEmpresa, @PathVariable Long id) {
+    
+    return repository.findById(id)
+      .map(empresa -> {
+        empresa.setNombre(newEmpresa.getNombre());
+        empresa.setDescripcion(newEmpresa.getDescripcion());
+        return repository.save(empresa);
+      })
+      .orElseGet(() -> {
+        newEmpresa.setId(id);
+        return repository.save(newEmpresa);
+      });
+  }
+
+  @DeleteMapping("/empresas/{id}")
+  void deleteEmpresa(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
 }
