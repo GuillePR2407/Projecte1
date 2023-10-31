88b56266a4808e741610fa4c4d89176d0dae63fc
GuillePR2407 <guillempr2003@gmail.com>
2023-10-31 14:54:47 +0100

    aaaaa

--- ProjecteSpring/src/main/java/com/example/demo/controller/EmpresaController.java
+++ ProjecteSpring/src/main/java/com/example/demo/controller/EmpresaController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,20 +23,41 @@ public class EmpresaController {
     @GetMapping("/{id}")
     public ResponseEntity<Empresa> consultarEmpresa(@PathVariable Long id) {
         // Implementa la lógica para consultar una empresa por su ID
+    	 Optional<Empresa> empresa = empresaRepository.findById(id);
+         if (empresa.isPresent()) {
+             return ResponseEntity.ok(empresa.get());
+         } else {
+             return ResponseEntity.notFound().build();
+         }
     }
 
     @PostMapping
     public ResponseEntity<Empresa> agregarEmpresa(@RequestBody Empresa empresa) {
         // Implementa la lógica para agregar una empresa
+    	Empresa nuevaEmpresa = empresaRepository.save(empresa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaEmpresa);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Empresa> modificarEmpresa(@PathVariable Long id, @RequestBody Empresa empresa) {
         // Implementa la lógica para modificar una empresa
+    	if (empresaRepository.existsById(id)) {
+            empresa.setId(id);
+            Empresa empresaModificada = empresaRepository.save(empresa);
+            return ResponseEntity.ok(empresaModificada);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> eliminarEmpresa(@PathVariable Long id) {
         // Implementa la lógica para eliminar una empresa
+    	if (empresaRepository.existsById(id)) {
+            empresaRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
