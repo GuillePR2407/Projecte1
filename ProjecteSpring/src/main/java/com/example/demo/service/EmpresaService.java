6601964705716b96c008a17b368c6ee850c9f48f
vestofan <vestofan14@gmail.com>
2023-11-24 16:07:43 +0100

    Modificado EmpresaService y EmpresaServiceTest

--- ProjecteSpring/src/main/java/com/example/demo/service/EmpresaService.java
+++ ProjecteSpring/src/main/java/com/example/demo/service/EmpresaService.java
@@ -1,19 +1,18 @@
 package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.bean.Empresa;
 import com.example.demo.bean.EmpresaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmpresaService {
 
     private final EmpresaRepository empresaRepository;
 
-    @Autowired
     public EmpresaService(EmpresaRepository empresaRepository) {
         this.empresaRepository = empresaRepository;
     }
@@ -25,6 +24,15 @@ public class EmpresaService {
     public List<Empresa> getAllEmpresas() {
         return empresaRepository.findAll();
     }
+
+    public Empresa getEmpresaById(Long empresaId) {
+        Optional<Empresa> optionalEmpresa = empresaRepository.findById(empresaId);
+        return optionalEmpresa.orElse(null);
+    }
+
+	public void deleteEmpresaById(Long empresaId) {
+		empresaRepository.deleteById(empresaId);
+	}
 
     // Otros métodos de servicio según sea necesario
 }
--- ProjecteSpring/src/main/java/com/example/demo/test/EmpresaServiceTest.java
+++ ProjecteSpring/src/main/java/com/example/demo/test/EmpresaServiceTest.java
@@ -1,59 +1,99 @@
 package com.example.demo.test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
 
 import com.example.demo.bean.Empresa;
 import com.example.demo.bean.EmpresaRepository;
 import com.example.demo.service.EmpresaService;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
 @SpringBootTest
 public class EmpresaServiceTest {
 
-    @Mock
+    @MockBean
     private EmpresaRepository empresaRepository;
 
-    @InjectMocks
-    private EmpresaService empresaService;
+    // Inject the EmpresaService with the mock repository
+    private final EmpresaService empresaService = new EmpresaService(empresaRepository);
 
     @Test
+    @DirtiesContext
     public void testCreateEmpresa() {
-        // Configurar el comportamiento esperado del repositorio de empresa
-        Empresa empresa = new Empresa("Empresa de prueba", "Descripción de la empresa de prueba");
-        when(empresaRepository.save(Mockito.any(Empresa.class))).thenReturn(empresa);
+        // Arrange
+        Empresa newEmpresa = new Empresa("NombreEmpresa", "DescripciónEmpresa");
+        when(empresaRepository.save(newEmpresa)).thenReturn(newEmpresa);
 
-        // Llamar al método del servicio
-        Empresa createdEmpresa = empresaService.createEmpresa(new Empresa());
+        // Act
+        Empresa savedEmpresa = empresaService.createEmpresa(newEmpresa);
 
-        // Verificar que la empresa se haya creado correctamente
-        assertEquals("Empresa de prueba", createdEmpresa.getNombre());
-        assertEquals("Descripción de la empresa de prueba", createdEmpresa.getDescripcion());
-        // Agregar más verificaciones según sea necesario
+        // Assert
+        assertNotNull(savedEmpresa);
+        assertEquals("NombreEmpresa", savedEmpresa.getNombre());
+        assertEquals("DescripciónEmpresa", savedEmpresa.getDescripcion());
+
+        // Verify that the save method was called once
+        verify(empresaRepository, times(1)).save(newEmpresa);
     }
 
     @Test
+    @DirtiesContext
     public void testGetAllEmpresas() {
-        // Configurar el comportamiento esperado del repositorio de empresa
-        Empresa empresa1 = new Empresa("Empresa 1", "Descripción 1");
-        Empresa empresa2 = new Empresa("Empresa 2", "Descripción 2");
-        when(empresaRepository.findAll()).thenReturn(Arrays.asList(empresa1, empresa2));
+        // Arrange
+        List<Empresa> empresas = new ArrayList<>();
+        empresas.add(new Empresa("Empresa1", "Descripción1"));
+        empresas.add(new Empresa("Empresa2", "Descripción2"));
+        when(empresaRepository.findAll()).thenReturn(empresas);
 
-        // Llamar al método del servicio
+        // Act
         List<Empresa> allEmpresas = empresaService.getAllEmpresas();
 
-        // Verificar que se hayan recuperado correctamente las empresas
+        // Assert
+        assertNotNull(allEmpresas);
         assertEquals(2, allEmpresas.size());
-        // Agregar más verificaciones según sea necesario
+
+        // Verify that the findAll method was called once
+        verify(empresaRepository, times(1)).findAll();
     }
 
-    // Agregar más pruebas según sea necesario
+    @Test
+    @DirtiesContext
+    public void testGetEmpresaById() {
+        // Arrange
+        Long empresaId = 1L;
+        Empresa empresa = new Empresa("NombreEmpresa", "DescripciónEmpresa");
+        when(empresaRepository.findById(empresaId)).thenReturn(Optional.of(empresa));
+
+        // Act
+        Empresa foundEmpresa = empresaService.getEmpresaById(empresaId);
+
+        // Assert
+        assertNotNull(foundEmpresa);
+        assertEquals("NombreEmpresa", foundEmpresa.getNombre());
+        assertEquals("DescripciónEmpresa", foundEmpresa.getDescripcion());
+
+        // Verify that the findById method was called once
+        verify(empresaRepository, times(1)).findById(empresaId);
+    }
+
+    @Test
+    @DirtiesContext
+    public void testDeleteEmpresaById() {
+        // Arrange
+        Long empresaId = 1L;
+
+        // Act
+        empresaService.deleteEmpresaById(empresaId);
+
+        // Verify that the deleteById method was called once
+        verify(empresaRepository, times(1)).deleteById(empresaId);
+    }
 }
