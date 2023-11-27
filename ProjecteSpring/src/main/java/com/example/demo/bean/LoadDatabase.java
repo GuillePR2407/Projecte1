c09c021edd9f4311543ddc92408e0ea41f8b3d1d
GuillePR2407 <guillempr2003@gmail.com>
2023-11-27 15:33:19 +0100

    aaa

--- ProjecteSpring/src/main/java/com/example/demo/bean/LoadDatabase.java
+++ ProjecteSpring/src/main/java/com/example/demo/bean/LoadDatabase.java
@@ -16,27 +16,34 @@ class LoadDatabase {
     CommandLineRunner initDatabase(EmpresaRepository empresaRepository, OfertaRepository ofertaRepository) {
         Empresa empresa1 = new Empresa("Giyesl", "klklklklkl");
         Empresa empresa2 = new Empresa("fransl", "klklklklkl");
+        Empresa empresa3 = new Empresa("fransl", "tonto");
 
         // Guardar las empresas primero
         empresaRepository.save(empresa1);
         empresaRepository.save(empresa2);
+        empresaRepository.save(empresa3);
 
         // Use the saved empresas to create ofertas
         Oferta oferta1 = new Oferta("Activa", "programador", "Descripción ", Sector.DAM, fechaRegistro, empresa1);
         Oferta oferta2 = new Oferta("Activa", "sistemas", "Descripción ç", Sector.ASIX, fechaRegistro, empresa1);
         Oferta oferta3 = new Oferta("Pendiente", "programador", "Descripción a", Sector.DAW, fechaRegistro, empresa2);
+        Oferta oferta4 = new Oferta("Pendiente", "programador", "Descripción a", Sector.DAW, fechaRegistro, empresa3);
+
 
         // Save the ofertas
         ofertaRepository.save(oferta1);
         ofertaRepository.save(oferta2);
         ofertaRepository.save(oferta3);
+        ofertaRepository.save(oferta4);
 
         return args -> {
             log.info("Preloading " + empresa1);
             log.info("Preloading " + empresa2);
+            log.info("Preloading " + empresa3);
             log.info("Preloading " + oferta1);
             log.info("Preloading " + oferta2);
             log.info("Preloading " + oferta3);
+            log.info("Preloading " + oferta4);
         };
     }
 }
