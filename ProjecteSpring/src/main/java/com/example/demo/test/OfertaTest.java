8e504f99d5be2d22e46c7658ca049b2bc00d9a58
vestofan <vestofan14@gmail.com>
2023-12-04 16:40:36 +0100

    modificados tests de las clases Oferta y Empresa

--- ProjecteSpring/src/main/java/com/example/demo/test/OfertaTest.java
+++ ProjecteSpring/src/main/java/com/example/demo/test/OfertaTest.java
@@ -15,40 +15,30 @@ public class OfertaTest {
 
     @Test
     public void testGettersAndSetters() {
-        // Crear una instancia de Oferta
-        Oferta oferta = new Oferta();
         
-        // Establecer valores
-        oferta.setId(1L);
-        oferta.setStatus(Status.ACTIVA);
-        oferta.setNom("Oferta1");
-        oferta.setDescripcio("Descripción1");
-        oferta.setSector(Sector.DAM);
-        Calendar fecha = Calendar.getInstance();
-        oferta.setRegistDate(fecha);
-        
-        // Crear una empresa para asociar con la oferta
+    	// Crear una empresa para asociar con la oferta
         Empresa empresa = new Empresa("Empresa1", "DescripciónEmpresa1");
-        oferta.setEmpresa(empresa);
         
+        // Crear una instancia de Oferta
+        Oferta oferta = new Oferta(Status.ACTIVA, "Oferta1", "Descripción1", Sector.DAM, Calendar.getInstance(), empresa);
+        
+        empresa.addOferta(oferta);        
         // Verificar que los valores se han establecido correctamente
-        assertEquals(1L, oferta.getId());
         assertEquals(Status.ACTIVA, oferta.getStatus());
         assertEquals("Oferta1", oferta.getNom());
         assertEquals("Descripción1", oferta.getDescripcio());
         assertEquals(Sector.DAM, oferta.getSector());
-        assertEquals(fecha, oferta.getRegistDate());
         assertEquals(empresa, oferta.getEmpresa());
     }
 
     @Test
     public void testGetEmpresa() {
-        // Crear una instancia de Oferta
-        Oferta oferta = new Oferta();
-        
+    	
         // Crear una empresa para asociar con la oferta
         Empresa empresa = new Empresa("Empresa2", "DescripciónEmpresa2");
-        oferta.setEmpresa(empresa);
+        
+        // Crear una instancia de Oferta
+        Oferta oferta = new Oferta(Status.ACTIVA, "Oferta1", "Descripción1", Sector.DAM, Calendar.getInstance(), empresa);
         
         // Verificar que el método getEmpresa devuelve el ID correcto de la empresa asociada
         assertEquals(empresa.getId(), oferta.getEmpresa());
