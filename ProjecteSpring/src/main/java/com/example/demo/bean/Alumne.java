70f6ce1d8c6cd265e0006a3871eefaf1621a69ed
vestofan <vestofan14@gmail.com>
2023-12-13 15:22:35 +0100

    comenzado sprint 3, añadir login y usuarios

---
+++ ProjecteSpring/src/main/java/com/example/demo/bean/Alumne.java
@@ -0,0 +1,49 @@
+package com.example.demo.bean;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USUARIOS_ADMINISTRADORES")
+public class Alumne {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NOMBRE", nullable = false, length = 100)
+    private String nombre;
+    
+    @Column(name = "CORREO", nullable = false, length = 100)
+    private String correo;
+
+    @Column(name = "CLAVE", nullable = false, length = 100)
+    private String clave;
+
+    
+    
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+}
