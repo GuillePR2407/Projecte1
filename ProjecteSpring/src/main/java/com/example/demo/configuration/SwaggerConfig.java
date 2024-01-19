0b2910e371b55f8cc611d20fbfb951cf0c94eefd
Francisco Jordan Garcia <franjoga24@gmail.com>
2024-01-19 13:35:32 +0100

    fixed swagger ui not working neither showing when not givin errors

--- ProjecteSpring/src/main/java/com/example/demo/configuration/SwaggerConfig.java
+++ ProjecteSpring/src/main/java/com/example/demo/configuration/SwaggerConfig.java
@@ -1,4 +1,14 @@
 package com.example.demo.configuration;
 
-public class SwaggerConfig {
-}
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@SecurityScheme(
+        type = SecuritySchemeType.HTTP,
+        name = "Bearer",
+        scheme = "bearer",
+        bearerFormat = "JWT")
+public class SwaggerConfig {}
